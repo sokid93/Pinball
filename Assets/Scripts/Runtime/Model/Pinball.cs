05963e309bb2ecb5898fa0284d95c8281bf2e471
Francisco Jose Soriano Ruiz <franbarrabajakh@gmail.com>
2024-03-26 19:09:39 +0100

    Test: default score is 0

--- Assets/Scripts/Runtime/Model/Pinball.cs
+++ Assets/Scripts/Runtime/Model/Pinball.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class Piball : MonoBehaviour
+public class Pinball
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int score;
 
-    // Update is called once per frame
-    void Update()
+    public Pinball(int score)
     {
-        
+
     }
 }
--- Assets/Scripts/Tests/DomainTests.cs
+++ Assets/Scripts/Tests/DomainTests.cs
@@ -6,20 +6,10 @@ using UnityEngine.TestTools;
 
 public class DomainTests
 {
-    // A Test behaves as an ordinary method
     [Test]
-    public void DomainsTestsSimplePasses()
+    public void testDefaultScore()
     {
-        // Use the Assert class to test conditions
-    }
-
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-    [UnityTest]
-    public IEnumerator DomainsTestsWithEnumeratorPasses()
-    {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
+        Pinball pinball = new Pinball(0);
+        Assert.AreEqual(0, pinball.score);
     }
 }
