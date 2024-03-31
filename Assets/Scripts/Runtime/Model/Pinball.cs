59a7f69be363e09904402d2a8d7828286253cd72
Francisco Jose Soriano Ruiz <franbarrabajakh@gmail.com>
2024-04-01 00:38:11 +0200

    Refactor: Changed the substraction of remainingBall from LoseBall to ReleaseBall->Three test failing (Game Over, Cannot Release Ball if there is one in play or there are no more)

--- Assets/Scripts/Runtime/Model/Pinball.cs
+++ Assets/Scripts/Runtime/Model/Pinball.cs
@@ -44,6 +44,7 @@ public class Pinball
     public void ReleaseBall()
     {
         if (HasBallsRemaining())
+            ballsRemaining -= 1;
             hasBall = true;
     }
 
@@ -55,7 +56,7 @@ public class Pinball
     public void LoseBall()
     {
         hasBall = false;
-        ballsRemaining -= 1;
+        
         if (!HasBallsRemaining())
             isGameOver = true;
     }
