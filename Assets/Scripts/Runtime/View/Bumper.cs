71fd0e6cb229b5c3bb3aee7cbe86adb1e65b9b11
Francisco Jose Soriano Ruiz <franbarrabajakh@gmail.com>
2024-04-03 16:21:03 +0200

    Feat: Bumpers bewhaviour and scene tweaks

---
+++ Assets/Scripts/Runtime/View/Bumper.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumper : MonoBehaviour
+{
+    Pinball pinballMachine;
+
+    private void Start()
+    {
+        pinballMachine = new Pinball(3);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Ball"))
+        {
+            Rigidbody ballRB = collision.rigidbody;
+            ballRB.AddExplosionForce(1000, collision.contacts[0].point, 1);
+            pinballMachine.AddPoints(10);            
+        }
+    }
+}
