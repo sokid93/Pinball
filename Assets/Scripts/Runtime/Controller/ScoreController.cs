7f4513b26442a023cc2bb91fad47ea5dbfe2fc8c
Francisco Jose Soriano Ruiz <franbarrabajakh@gmail.com>
2024-04-12 16:36:28 +0200

    Feat: Score Controller no manages score addition and display (Score Panel doesn't need the model now)

--- Assets/Scripts/Runtime/Controller/ScoreController.cs
+++ Assets/Scripts/Runtime/Controller/ScoreController.cs
@@ -3,14 +3,22 @@
 public class ScoreController
 {
     private readonly Pinball pinball;
+    private readonly sadlñkfjñs pointsDisplay;
 
-    public ScoreController(Pinball pinball)
+    public ScoreController(Pinball pinball, sadlñkfjñs pointsDisplay)
     {
         this.pinball = pinball;
+        this.pointsDisplay = pointsDisplay;
+    }
+
+    public void InitializeScore()
+    {
+        pointsDisplay.DisplayPoints(pinball.Score);
     }
 
     public void EarnPoints(int points)
     {
         pinball.AddPoints(points);
+        pointsDisplay.DisplayPoints(pinball.Score);
     }
 }
