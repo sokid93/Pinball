be5ca8047f10d46925e766956e6fdd15a4fbf737
Francisco Jose Soriano Ruiz <franbarrabajakh@gmail.com>
2024-05-29 11:47:39 +0200

    Feat: added Save Game State controller

---
+++ Assets/Scripts/Runtime/Controller/SaveGameState.cs
@@ -0,0 +1,24 @@
+
+
+public class SaveGameState
+{
+    private readonly GameRepository Repository;
+    private readonly Pinball Pinball;
+
+    public SaveGameState(GameRepository repository, Pinball pinball)
+    {
+        Repository = repository;
+        Pinball = pinball;
+    }
+
+    public void Run()
+    {
+        Repository.SaveGameState(Pinball);
+    }
+}
+
+public interface GameRepository
+{
+    void SaveGameState(Pinball pinball);
+    void LoadGameState();
+}
