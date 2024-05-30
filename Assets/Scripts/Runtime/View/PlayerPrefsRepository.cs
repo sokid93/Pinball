80f5250985f87f4d8b4a266e9604747fc6ba37bb
Francisco Jose Soriano Ruiz <franbarrabajakh@gmail.com>
2024-05-30 13:18:51 +0200

    Feat: added Player Prefs repository

---
+++ Assets/Scripts/Runtime/View/PlayerPrefsRepository.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsRepository : GameRepository
+{
+    public Pinball LoadGameState()
+    {
+        int ballsRemaining = PlayerPrefs.GetInt("BallsRemaining", new Pinball(3).BallsRemaining);
+        int score = PlayerPrefs.GetInt("Score", new Pinball().Score);
+        return new Pinball(ballsRemaining, score);
+    }
+
+    public void SaveGameState(Pinball pinball)
+    {
+        PlayerPrefs.SetInt("BallsRemaining", pinball.BallsRemaining);
+        PlayerPrefs.SetInt("Score", pinball.Score);
+    }
+}
