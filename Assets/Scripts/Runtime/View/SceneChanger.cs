6b4326e9f7dcc7f046020a8a7e318ce3c04cefd7
Francisco Jose Soriano Ruiz <franbarrabajakh@gmail.com>
2024-05-31 11:20:51 +0200

    Feat: save game state and load new scene

---
+++ Assets/Scripts/Runtime/View/SceneChanger.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void OnDestroy()
+    {
+        SaveGameState();
+    }
+
+    private void SaveGameState() => FindObjectOfType<Dependencies>().SaveGame.Run();
+}
