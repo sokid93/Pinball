65ab9bc90f20135185bf378d8e956a6b4ceb2d39
Francisco Jose Soriano Ruiz <franbarrabajakh@gmail.com>
2024-06-16 11:34:37 +0200

    refactor: scene loader name

--- Assets/Scripts/Runtime/View/SceneLoader.cs
+++ Assets/Scripts/Runtime/View/SceneLoader.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneChanger : MonoBehaviour
+public class SceneLoader : MonoBehaviour
 {
     [SerializeField] int NextSceneId;
 
