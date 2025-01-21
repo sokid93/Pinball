6f93b7a5faeadcff9303dcfef53e969bfa9c829c
Francisco Jose Soriano Ruiz <franbarrabajakh@gmail.com>
2025-01-21 13:26:04 +0100

    fix: build path

--- Assets/Runtime/BuildScript.cs
+++ Assets/Runtime/BuildScript.cs
@@ -1,13 +1,11 @@
 using UnityEditor;
-using UnityEngine;
-using System.IO;
 
 public class BuildScript
 {
     public static void PerformBuild()
     {
         BuildPlayerOptions buildOptions = new BuildPlayerOptions();
-        buildOptions.locationPathName = "Builds";
+        buildOptions.locationPathName = "Builds/Pinball.exe";
         buildOptions.scenes = new string[] { "Assets/SampleScene.unity", "Assets/SecondLevel.unity", "Assets/ThirdLevelA.unity", "Assets/ThirdLevelB.unity" };
         buildOptions.target = BuildTarget.StandaloneWindows;
         buildOptions.options = BuildOptions.None;
