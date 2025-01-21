5d65e4641e71942147288b500be4289f6ef2f582
Francisco Jose Soriano Ruiz <franbarrabajakh@gmail.com>
2025-01-21 11:59:24 +0100

    fix: hardcoded build path

--- Assets/Runtime/BuildScript.cs
+++ Assets/Runtime/BuildScript.cs
@@ -1,14 +1,19 @@
 using UnityEditor;
+using UnityEngine;
+using System.IO;
 
 public class BuildScript
 {
     public static void PerformBuild()
     {
-        string buildPath = EditorUtility.SaveFolderPanel("Assets/Builds", "","");
+        string buildPath = "Builds";
+        if (!System.IO.Directory.Exists(buildPath))
+        {
+            System.IO.Directory.CreateDirectory(buildPath);
+        }
+
         string[] scenes = new string[] { "Assets/SampleScene.unity", "Assets/SecondLevel.unity", "Assets/ThirdLevelA.unity", "Assets/ThirdLevelB.unity" };
 
         BuildPipeline.BuildPlayer(scenes, buildPath + "/Pinball.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
-
-        FileUtil.CopyFileOrDirectory("Assets/Templates/Readme.txt", buildPath + "Readme.txt");
     }
 }
