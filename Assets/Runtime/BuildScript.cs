cc4776169ac642956cb80dc7cd4944b9a454129f
Francisco Jose Soriano Ruiz <franbarrabajakh@gmail.com>
2025-01-22 00:38:46 +0100

    fix: simplify build script

--- Assets/Runtime/BuildScript.cs
+++ Assets/Runtime/BuildScript.cs
@@ -1,10 +1,8 @@
 using UnityEditor;
 using UnityEngine;
-using UnityEditor.Build.Reporting;
 
 public class BuildScript
 {
-    [MenuItem("Build/Build WebGL")]
     public static void PerformBuild()
     {
         BuildPlayerOptions buildOptions = new BuildPlayerOptions();
@@ -13,17 +11,6 @@ public class BuildScript
         buildOptions.target = BuildTarget.WebGL;
         buildOptions.options = BuildOptions.None;
 
-        BuildReport report = BuildPipeline.BuildPlayer(buildOptions);
-        BuildSummary summary = report.summary;
-
-        if(summary.result == BuildResult.Succeeded)
-        {
-            Debug.Log("Build succeded: " + summary.totalSize + " bytes");
-        }
-
-        if (summary.result == BuildResult.Failed)
-        {
-            Debug.Log("Build failed");
-        }
+        BuildPipeline.BuildPlayer(buildOptions);
     }
 }
