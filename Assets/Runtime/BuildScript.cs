3fbf7ba611e7cbdd96d5b2060836caf34b9d9fe9
Francisco Jose Soriano Ruiz <franbarrabajakh@gmail.com>
2025-01-21 23:46:26 +0100

    feat: added build menu and summary of the build

--- Assets/Runtime/BuildScript.cs
+++ Assets/Runtime/BuildScript.cs
@@ -1,15 +1,29 @@
 using UnityEditor;
+using UnityEngine;
+using UnityEditor.Build.Reporting;
 
 public class BuildScript
 {
+    [MenuItem("Build/Build WebGL")]
     public static void PerformBuild()
     {
         BuildPlayerOptions buildOptions = new BuildPlayerOptions();
-        buildOptions.locationPathName = "Builds/Pinball.exe";
         buildOptions.scenes = new string[] { "Assets/SampleScene.unity", "Assets/SecondLevel.unity", "Assets/ThirdLevelA.unity", "Assets/ThirdLevelB.unity" };
-        buildOptions.target = BuildTarget.StandaloneWindows;
+        buildOptions.locationPathName = "Builds/WebGL";
+        buildOptions.target = BuildTarget.WebGL;
         buildOptions.options = BuildOptions.None;
 
-        BuildPipeline.BuildPlayer(buildOptions);
+        BuildReport report = BuildPipeline.BuildPlayer(buildOptions);
+        BuildSummary summary = report.summary;
+
+        if(summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
     }
 }
